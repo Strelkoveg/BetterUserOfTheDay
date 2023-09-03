a29c5ae870917a536c6ae9dc0c026b4716c8ac2c
Strelkoveg <64949377+Strelkoveg@users.noreply.github.com>
2023-09-03 22:21:50 +0400

    Update setup.sql

--- setup_db/setup.sql
+++ setup_db/setup.sql
@@ -1,7 +1,7 @@
--- create the databases
+-- create the database
 CREATE DATABASE IF NOT EXISTS test_database;
 
--- create the users for each database
+-- create the user for database
 CREATE USER 'bot'@'%' IDENTIFIED BY '11111';
 GRANT CREATE, ALTER, INDEX, LOCK TABLES, REFERENCES, UPDATE, DELETE, DROP, SELECT, INSERT ON `test_database`.* TO 'bot'@'%';
 
